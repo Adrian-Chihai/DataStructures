964c0a20eab3b47241cb7695b214441ffd6fd87b
Adrian Chihai <cadrian1602@yahoo.com>
2023-11-08 12:53:29 +0200

    w

---
+++ JavaDSA/src/LeetCode/RottenOranges.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class RottenOranges {
+    public static void main(String[] args) {
+        System.out.println(orangesRotting(new int[][]{{2,1,1},{1,1,0},{0,1,1}}));
+    }
+
+    public static int orangesRotting(int[][] grid) {
+        int min = 0;
+        int[] rottenPos = findRotten(grid);
+
+        if (rottenPos.length == 0){
+            return -1;
+        }
+    }
+
+    public static int[] findRotten(int[][] grid) {
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == 2){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
