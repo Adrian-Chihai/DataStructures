70cf3b5a5ac7b9bb117e66a8ea613c906e55f712
ChihaiAdrian <cadrian1602@yahoo.com>
2023-11-19 13:36:03 +0200

    LL update

--- JavaDSA/src/UdemyCourse/DataStructures/LinkedList/Main.java
+++ JavaDSA/src/UdemyCourse/DataStructures/LinkedList/Main.java
@@ -3,10 +3,12 @@ package UdemyCourse.DataStructures.LinkedList;
 public class Main {
     public static void main(String[] args) {
         MyLinkedList linkedList = new MyLinkedList(4);
+        for (int i = 0; i < 10; i++){
+            linkedList.prepend(i*2);
+        }
         linkedList.printList();
+        System.out.println();
 
-        linkedList.removeLast();
-        linkedList.printList();
-
+        System.out.println(linkedList.getNode(4).val);
     }
 }
--- JavaDSA/src/UdemyCourse/DataStructures/LinkedList/MyLinkedList.java
+++ JavaDSA/src/UdemyCourse/DataStructures/LinkedList/MyLinkedList.java
@@ -25,17 +25,45 @@ public class MyLinkedList {
         return length;
     }
 
+    public void prepend(int value) {
+        Node node = new Node(value);
+        if (length == 0){
+            head = node;
+            tail = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+        length++;
+    }
+
     public void append(int value) {
         Node node = new Node(value);
         if(length == 0) {
             head = node;
             tail = node;
-            length = 1;
         } else {
             tail.next = node;
             tail = node;
-            length++;
         }
+        length++;
+    }
+
+    public Node removeFirst(){
+        if (length == 0){
+            return null;
+        }
+
+        Node node = head;
+        head = head.next;
+        node.next = null;
+        length--;
+
+        if (length == 0){
+            tail = null;
+        }
+
+        return node;
     }
 
     public Node removeLast(){
@@ -62,13 +90,28 @@ public class MyLinkedList {
 
         return temp;
     }
+
+    public Node getNode(int index){
+        if(index == 0){
+            return head;
+        } else if(index == length - 1){
+            return tail;
+        } else if(index < 0 || index > length - 1){
+            return null;
+        }
+        Node node = head;
+        for (int i = 0; i < index; i++){
+            node = node.next;
+        }
+        return node;
+    }
+
     public void printList() {
         Node node = head;
         while (node != null){
             System.out.print(node.val + " ");
             node = node.next;
         }
-        System.out.println();
     }
 
     class Node {
